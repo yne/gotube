d724254816d742c7bbbb8face1ccb8d5df4264d8
biscottealacrevette@gmail.com <biscottealacrevette@gmail.com@9aa743bc-fd49-11de-8802-f5e385a79211>
2010-02-24 23:06:53 +0000

    added fmt changing support to youtube

--- trunk/biscotte/YouTube.js
+++ trunk/biscotte/YouTube.js
@@ -1,8 +1,29 @@
 var YouTube = new Object();
-YouTube.rev           = 3;
-YouTube.SearchDesc    = 
+YouTube.rev           = 4;
+YouTube.SearchDesc    = 'Search "&fmt=5" to SD "&fmt=18" HQ ...'
 YouTube.Name          = "YouTube";
+YouTube.opt           = "";
 YouTube.Search        = function (keyword, page){
+	if(keyword.match(/^&/)){alert(keyword.match(/^&(.*)/)[1]+' is now used');YouTube.opt=keyword;return;}
+ /* FMT table (FHD sample:mR04JNbSPyo)
+		13:176*144 (AMR? 2*8kHz | S263) << overide aspect ratio
+		17:176*144 (AAC 1*22kHz 30Kbs|MPEG4)
+		18:480*[360/270/272]
+		22:1280*720
+		32:1920*1080
+		34:640*368 (AAC 2*44kHz|H264)
+		35:864*480 (AAC 2*44kHz|H264)
+		36:320x240 (AAC 1*22kHz|MPEG4)
+		37:1920*1080 (AAC 2*44kHz 130Kbps)
+	//i've tried to play :
+		5>YUV problem
+		13>no sound + freez
+		17>wrong Video fps + YUV problem + wrong Andio freq
+		18>ME Video too slow (good for listening Audio with static video)
+		34>freez
+		35>freez
+		36>wrong Video fps + wrong Andio freq
+ */
  var result = new Object();
  result.bypage    = 50;// modifiable
  result.start     = (page-1)*result.bypage+1;
@@ -23,8 +44,8 @@ YouTube.Search        = function (keyword, page){
   video.MylistCount   = ext("favoriteCount='")*1;
   video.ViewCount     = ext("viewCount='")*1;
   video.ThumbnailURL  = 'http://i.ytimg.com/vi/'+video.id+'/default.jpg';
-  video.SaveFilename  = video.Title+".flv"
-  video.URL           = '"http://www.youtube.com/get_video?video_id='+video.id+'&t="+GetContents("http://www.youtube.com/get_video_info?video_id='+video.id+'&el=embedded&ps=default&eurl=").match("&token=(.+?)&")[1]';
+  video.SaveFilename  = video.Title+".mp4"
+  video.URL           = '"http://www.youtube.com/get_video?video_id='+video.id+'&t="+GetContents("http://www.youtube.com/get_video_info?video_id='+video.id+'&el=embedded&ps=default&eurl=").match("&token=(.+?)&")[1]+"'+YouTube.opt+'"';
   result.VideoInfo.push(video);
  }
  result.end       = result.start-1+result.VideoInfo.length;
