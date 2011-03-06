af3716f530eaaee58d8b0877ab711439671b4b37
yne <yne@users.noreply.github.com>
2011-03-06 02:33:49 +0000

    updated due to new search query and new flash players interface

--- crevette/Motherless.js
+++ crevette/Motherless.js
@@ -1,25 +1,30 @@
 var Motherless = new Object();
-Motherless.rev        = 1;
+Motherless.rev        = 2;
 Motherless.SearchDesc =
 Motherless.Name       = "Motherless";
 Motherless.Search     = function (keyword, page){
  var result = new Object();
  result.bypage    = 41;//static
  result.start     = (page-1)*result.bypage+1;
- c=GetContents('http://motherless.com/search/videos?page='+page+'&search='+escape(keyword));
+ c=GetContents('http://motherless.com/search/videos?page='+page+'&q='+escape(keyword));
  result.total     = -1;
  result.VideoInfo = new Array();
+ p=c.indexOf("thumbnail mediatype_video");
  while(p=c.indexOf("thumbnail-img-wrap",p)+1){
   video = new Object();
   video.attr          = 3;
   video.id            = ext('<a href="');
   video.Title         = ext('title="');
-  video.ThumbnailURL  = ext('url(');
-  video.ViewCount     = ext('right ellipsis">',' ')*1;
-  video.URL           = 'GetContents("'+video.id+'").ext("flashvars\',\'file=","&")';//
+//  video.ThumbnailURL  = ext('url(');
+  video.ViewCount     = ext('right ellipsis">',' ').replace(/,/g,"");
+	video.URL	          = 'Motherless.play("'+video.id+'")';
   result.VideoInfo.push(video);
  }
  result.end       = result.start-1+result.VideoInfo.length;
  return result;
+}
+Motherless.play = function(url){
+	c=GetContents(url);p=0;
+	return ext("'file', '");
 }
 SiteList.push(Motherless);
