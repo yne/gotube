8fa331442472fb2dc8dda23e2d00d0b7e83fd9fd
biscottealacrevette@gmail.com <biscottealacrevette@gmail.com@9aa743bc-fd49-11de-8802-f5e385a79211>
2012-03-05 20:56:05 +0000

    testing

--- trunk/biscotte/RuTube.js
+++ trunk/biscotte/RuTube.js
@@ -9,24 +9,23 @@ RuTube.Search      = function(keyword, page){
  c=GetContents('http://rutube.ru/search.html?search='+escape(keyword)+';p='+(page-1));
  result.total     = -1;
  result.VideoInfo = new Array();
- while(p=c.indexOf('<table width="100"',p)+1){
+ while(p=c.indexOf('<div class="thumb">',p)+1){
   video = new Object();
   video.attr          = 3;
-	video.ThumbnailURL  = ext('<td style="background: url(');
-  video.id            = ext(".html?v=",'"');
-  video.Title         = ext('title="');
-  video.Description   = (ext("width:","p")*100)/(16*5)+'%';
-	video.LengthSeconds = ext("absmiddle>&nbsp;",':')*60+ext(':','<')*1;
-  video.ViewCount     = ext('">&nbsp;<b>')*1;
-  video.CommentCount  = ext("<b>")*1;
+	video.ThumbnailURL  = ext('src="');
+  video.Title         = ext('alt="');
+  video.id            = ext("/tracks/",'.');
+  video.LengthSeconds = ext('">',':')*60+ext(':','<')*1;
+  video.Description   = ext('date">');
+	video.ViewCount     = ext('">')*1;
   video.SaveFilename  = video.Title+".flv"
-  video.URL           = 'RuTube.play("'+video.id+'")';
+  video.URL           = 'RuTube.play("'+video.ThumbnailURL.match(/\w\/([a-z0-9]+)[.-]/)[1]+'")';
   result.VideoInfo.push(video);
  }
  result.end       = result.start-1+result.VideoInfo.length;
  return result;
 }
 RuTube.play      = function(id){
-return GetContents('http://bl.rutube.ru/'+id+'.xml').match(/CDATA\[(.*?)\]/)[1];
+  return GetContents('http://bl.rutube.ru/'+id+'.xml').match(/CDATA\[(.*?)\]/)[1];
 }
 SiteList.push(RuTube);
